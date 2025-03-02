b5a03b61ab195feb4d92a2811a6fd7de179dd218
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2025-03-02 19:32:18 +0100

    feat: show logged account information

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -2,16 +2,19 @@ import styles from './index.module.css'
 import { useCredentialStore } from '../../state/AuthState'
 import MobileNav from '../../components/MobileNav'
 
-
 export default function Feed() {
-	const { username, password } = useCredentialStore()
+	const { account } = useCredentialStore()
 	return (
 		<div className={styles.container}>
 			<header className={styles.header}></header>
 			<main className={styles.main}>
 				<h1>Feed</h1>
-				<div>Username: {username}</div>
-				<div>Password: {password && password}</div>
+				<div className='truncate max-w-xs overflow-hidden overflow-ellipsis whitespace-nowrap'>
+					Account Public Key: {account?.accountPublicKey}
+				</div>
+				<div className='truncate max-w-xs overflow-hidden overflow-ellipsis whitespace-nowrap'>
+					Block Public key: {account?.blockPublicKey}
+				</div>
 			</main>
 			<MobileNav />
 		</div>
